01ad7e55cb36f813f68b32e9a2794fc25202d43d
Andrew Furth <andrew.furth@gmail.com>
2016-05-28 14:32:36 -0700

    search query display titles and years of

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,9 @@ app.controller("MainController", function($scope, $http, $location) {
   $scope.view = {};
   $scope.searchMovies = function() {
     $http.get('http://www.omdbapi.com/?s='+$scope.view.searchText+'&type=movie&page=1&r=json').then(function(res) {
+      // Cpature previous query string (sets up to clear search bar)
       $scope.view.queryString = $scope.view.searchText;
+      $scope.view.searchResults = res.data;
       $scope.view.searchText = "";  
       $scope.searchForm.$setPristine();
       $location.path('/searchResults')
