28edb0a9d66d556f389d3965104f448b1f11447f
Andrew Furth <andrew.furth@gmail.com>
2016-05-28 14:09:33 -0700

    search results header displays query; search bar clears after search executes

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,11 @@
-app.controller("MainController", function($scope, $http) {
+app.controller("MainController", function($scope, $http, $location) {
   $scope.view = {};
   $scope.searchMovies = function() {
     $http.get('http://www.omdbapi.com/?s='+$scope.view.searchText+'&type=movie&page=1&r=json').then(function(res) {
+      $scope.view.queryString = $scope.view.searchText;
+      $scope.view.searchText = "";  
+      $scope.searchForm.$setPristine();
+      $location.path('/searchResults')
       console.log(res);
     })
   }
